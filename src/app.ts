a5d7ef33c5c73892331523d878fdd312810f5538
takoba <plane25@gmail.com>
2024-06-28 21:32:08 +0900

    :rotating_light: Exec `npx eslint . --fix`

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 // @TODO This is mock object. please implement your app.
 const App = () => ({
-  start: () => console.log('exec app.start()')
+  start: () => console.log('exec app.start()'),
 })
 export default App
